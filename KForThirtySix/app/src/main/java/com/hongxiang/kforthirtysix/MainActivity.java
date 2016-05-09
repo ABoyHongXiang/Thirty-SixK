08a972b108d9ebe62422f268a7689a70c5eefe3d
hongxiang <916634913@qq.com>
2016-05-09 21:16:52 +0800

    完成Fragment

--- KForThirtySix/app/src/main/java/com/hongxiang/kforthirtysix/MainActivity.java
+++ KForThirtySix/app/src/main/java/com/hongxiang/kforthirtysix/MainActivity.java
@@ -1,13 +1,61 @@
 package com.hongxiang.kforthirtysix;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+import com.hongxiang.kforthirtysix.fragment.FoundFragment;
+import com.hongxiang.kforthirtysix.fragment.InvestFragment;
+import com.hongxiang.kforthirtysix.fragment.MineFragment;
+import com.hongxiang.kforthirtysix.fragment.NewsFragment;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends FragmentActivity {
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
+    private MainAdapter mainAdapter;
+    private List<Fragment> fragmentList;
+    private DrawerLayout drawerLayout;
+    private ListView menuListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        viewPager = (ViewPager) findViewById(R.id.main_viewpager);
+        tabLayout = (TabLayout) findViewById(R.id.main_tablayout);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        menuListView = (ListView) findViewById(R.id.menu_listview);
+        initFragment();
+        mainAdapter = new MainAdapter(getSupportFragmentManager());
+        mainAdapter.setFragmentList(fragmentList);
+        viewPager.setAdapter(mainAdapter);
+        tabLayout.setupWithViewPager(viewPager);
+        initTab();
     }
+
+    private void initFragment() {
+        fragmentList = new ArrayList<>();
+        fragmentList.add(new NewsFragment());
+        fragmentList.add(new InvestFragment());
+        fragmentList.add(new FoundFragment());
+        fragmentList.add(new MineFragment());
+
+    }
+
+    private void initTab() {
+        tabLayout.getTabAt(0).setIcon(R.drawable.table_selector_news);
+        tabLayout.getTabAt(1).setIcon(R.drawable.table_selector_invest);
+        tabLayout.getTabAt(2).setIcon(R.drawable.table_selector_found);
+        tabLayout.getTabAt(3).setIcon(R.drawable.table_selsetor_mine);
+
+    }
+
 }
